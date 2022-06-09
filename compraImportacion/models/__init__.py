096e3a053dfc33494652b63c07c3a52ab2f2bb94
mserrate1980 <marcelo.serrate@alphasys.com.bo>
2022-06-09 18:39:53 -0400

    limpia

--- compraImportacion/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#from . import account_move
-from . import purchase_order
-from . import res_partner
-from . import purchaseOL
-from . import tracking
-
-
--- custom_crm/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sale_order
-from . import crm_lead
-from . import purchase_order
